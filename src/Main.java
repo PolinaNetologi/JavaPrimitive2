2ca2a519c3417fcc5b07ddcb65fec6d7cf42a1f8
Polina <pzmujdeckaa@gmail.com>
2023-04-04 20:39:24 +0500

    JavaPrimitive2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int initialBalance = 100;
+        int addent = 1100;
+
+        int bonus = 11;
+
+        if (addent > 1000) {
+            bonus = addent / 100;
+
+        }
+
+        int finalBalance = initialBalance + addent + bonus;
+        System.out.println("Итог: " + finalBalance);
+        System.out.println("Бонус: " + bonus);
     }
 }
